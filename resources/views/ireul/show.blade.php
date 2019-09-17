2a3ada10cbf70077a9879d6f4ff54050ffdd02ce
arumikan <arumikan1234@gmail.com>
2019-09-17 17:25:01 +0900

    アクティブ表示切り替え

--- resources/views/ireul/show.blade.php
+++ resources/views/ireul/show.blade.php
@@ -41,30 +41,31 @@ Drink MAGI
 Q1. つよさは？
 </p>
 <p>
-<div class="row btn_group">
-    <div class="btn_selectable col align-middle"><label for="yowai" class="col"><input type="radio" class="d-none" name="q1" value="1" id="yowai">弱い</label></div>
-    <div class="btn_selectable col align-middle"><label for="ynt" class="col"><input type="radio" class="d-none" name="q1" value="3" id="ynt">ふつう</label></div>
-    <div class="btn_selectable col align-middle"><label for="tuyoi" class="col"><input type="radio" class="d-none" name="q1" value="5" id="tuyoi">強い</label></div>
+
+<div class="row btn_group" id="first">
+    <div class="btn_selectable col align-middle"><label for="yowai" class="col"><input type="radio" class="d-none" name="q1" value="1" id="yowai" onclick="changeQ1Active(this)">弱い</label></div>
+    <div class="btn_selectable col align-middle"><label for="ynt" class="col"><input type="radio" class="d-none" name="q1" value="3" id="ynt" onclick="changeQ1Active(this)">ふつう</label></div>
+    <div class="btn_selectable col align-middle"><label for="tuyoi" class="col"><input type="radio" class="d-none" name="q1" value="5" id="tuyoi" onclick="changeQ1Active(this)">強い</label></div>
 </div>
 </p>
 <p>
 Q2. 味は？
 </p>
 <p>
-<div class="row btn_group">
-    <div class="btn_selectable col align-middle"><label for="amai" class="col"><input type="radio" class="d-none" name="q2" value="1" id="amai">甘い</label></div>
-    <div class="btn_selectable col align-middle"><label for="fu" class="col"><input type="radio" class="d-none" name="q2" value="3" id="fu">ふつう</label></div>
-    <div class="btn_selectable col align-middle"><label for="karai" class="col"><input type="radio" class="d-none" name="q2" value="5" id="karai">辛い</label></div>
+<div class="row btn_group" id="second">
+    <div class="btn_selectable col align-middle"><label for="amai" class="col"><input type="radio" class="d-none" name="q2" value="1" id="amai" onclick="changeQ2Active(this)">甘い</label></div>
+    <div class="btn_selectable col align-middle"><label for="fu" class="col"><input type="radio" class="d-none" name="q2" value="3" id="fu" onclick="changeQ2Active(this)">ふつう</label></div>
+    <div class="btn_selectable col align-middle"><label for="karai" class="col"><input type="radio" class="d-none" name="q2" value="5" id="karai" onclick="changeQ2Active(this)">辛い</label></div>
 </div>
 </p>
 <p>
 Q3. 後味は？
 </p>
 <p>
-<div class="row btn_group">
-    <div class="btn_selectable col align-middle"><label for="kiri" class="col"><input type="radio" class="d-none" name="q3" value="1" id="kiri">キリッと</label></div>
-    <div class="btn_selectable col align-middle"><label for="futuu" class="col"><input type="radio" class="d-none" name="q3" value="3" id="futuu">ふつう</label></div>
-    <div class="btn_selectable col align-middle"><label for="mattari" class="col"><input type="radio" class="d-none" name="q3" value="5" id="mattari">まったり</label></div>
+<div class="row btn_group" id="third">
+    <div class="btn_selectable col align-middle"><label for="kiri" class="col"><input type="radio" class="d-none" name="q3" value="1" id="kiri" onclick="changeQ3Active(this)">キリッと</label></div>
+    <div class="btn_selectable col align-middle"><label for="futuu" class="col"><input type="radio" class="d-none" name="q3" value="3" id="futuu" onclick="changeQ3Active(this)">ふつう</label></div>
+    <div class="btn_selectable col align-middle"><label for="mattari" class="col"><input type="radio" class="d-none" name="q3" value="5" id="mattari" onclick="changeQ3Active(this)">まったり</label></div>
 </div>
 </p>
 <p>
@@ -76,4 +77,29 @@ Q3. 後味は？
 @csrf
 </form>
 </body>
+<script>
+function changeQ1Active(button){
+    let obj = document.querySelectorAll("#first > div");
+    for(let idx = 0 ; idx < 3 ; idx++){
+        obj[idx].classList.remove('btn_active');
+    };
+    button.parentNode.parentNode.classList.add('btn_active');
+}
+
+function changeQ2Active(button){
+    let obj = document.querySelectorAll("#second > div");
+    for(let idx = 0 ; idx < 3 ; idx++){
+        obj[idx].classList.remove('btn_active');
+    };
+    button.parentNode.parentNode.classList.add('btn_active');
+}
+
+function changeQ3Active(button){
+    let obj = document.querySelectorAll("#third > div");
+    for(let idx = 0 ; idx < 3 ; idx++){
+        obj[idx].classList.remove('btn_active');
+    };
+    button.parentNode.parentNode.classList.add('btn_active');
+}
+</script>
 </html>
